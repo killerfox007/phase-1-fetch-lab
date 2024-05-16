92e95abbf441c46700506d150fe94cde1e06ae64
Kaleb Fox <kalebfox7@yahoo.com>
2024-05-16 16:53:42 -0400

    done

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+ function fetchBooks() {
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(resp =>  resp.json())
   
-}
+  .then(data => {
+    renderBooks(data)
+  }) 
+  }
+
+
+// function fetchBooks(){
+// fetch("https://anapioficeandfire.com/api/books")
+// .then(response => {
+//     return response.json()
+// })
+// .then(blogs => {
+//   renderBlogs(blogs)
+// })
+// }
+
+
 
 function renderBooks(books) {
+  // console.log(books)
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
+
   });
 }
 
